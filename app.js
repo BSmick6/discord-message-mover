cbc56a480153311ff5119dc62f836e11787e3657
Brock <bsmick97@gmail.com>
2024-05-29 09:52:55 -0400

    declare command and msg component

--- app.js
+++ app.js
@@ -52,6 +52,48 @@ app.post('/interactions', async function (req, res) {
         },
       });
     }
+
+    // "move" message command
+    if (name === 'move') {
+      console.log('appcomm', data.resolved);
+      // get data
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: 'we in the msg command',
+          components: [
+            {
+              type: MessageComponentTypes.ACTION_ROW,
+              components: [
+                {
+                  type: MessageComponentTypes.CHANNEL_SELECT,
+                  custom_id: 'channel_select'
+                }
+              ]
+            }
+          ]
+        }
+      })
+    }
+  }
+
+  if (type === InteractionType.MESSAGE_COMPONENT) {
+    const {channels} = data.resolved
+    const channel_id = Object.keys(channels)[0]
+    console.log('msgcomp channel', data.resolved);
+    try {
+      await DiscordRequest(`channels/${channel_id}/messages`, {
+        method: 'POST',
+      })
+    } catch (error) {
+      
+    }
+    return res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: 'you selected ' + Object.values(data.resolved.channels)[0].name
+      }
+    })
   }
 });
 
